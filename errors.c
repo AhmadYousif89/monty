c7542f99a399759c6afac7ac78e14d8e9038fa16
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-18 19:00:50 +0200

    update: errors.c file

--- errors.c
+++ errors.c
@@ -7,13 +7,16 @@
 void print_error(int error_code, ...)
 {
 	va_list val;
+	int ln;
 
 	va_start(val, error_code);
 	if (error_code == 1)
 		fprintf(stderr, "Error: Can't open file %s\n", va_arg(val, char *));
 	if (error_code == 2)
-		fprintf(stderr, "L%d: unknown instruction %s\n",
-				va_arg(val, int), va_arg(val, char *));
+	{
+		ln = va_arg(val, int);
+		fprintf(stderr, "L%d: unknown instruction %s\n", ln, va_arg(val, char *));
+	}
 	if (error_code == 3)
 		fprintf(stderr, "Error: malloc failed\n");
 	if (error_code == 4)
@@ -23,10 +26,12 @@ void print_error(int error_code, ...)
 	if (error_code == 6)
 		fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(val, int));
 	if (error_code == 7)
-		fprintf(stderr, "L%d: can't %s, stack too short\n",
-				va_arg(val, unsigned int), va_arg(val, char *));
+	{
+		ln = va_arg(val, int);
+		fprintf(stderr, "L%d: can't %s, stack too short\n", ln, va_arg(val, char *));
+	}
 	if (error_code == 8)
-		fprintf(stderr, "L%d: division by zero\n", va_arg(val, unsigned int));
+		fprintf(stderr, "L%d: division by zero\n", va_arg(val, int));
 	if (error_code == 9)
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", va_arg(val, int));
 	if (error_code == 10)
